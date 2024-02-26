b9329215a3f3e9aa61b01657949ba2c5ba1eaf44
e1rhyme <emoakpo.onerhime@gmail.com>
2024-02-26 11:41:26 +0100

    version check

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,7 +7,7 @@ import deleteContactView from "./view/deleteContactView.js";
 
 // Handles loading of contacts in the view
 function controlLoadContacts(condition) {
-  console.log("DebugginG");
+  console.log("debugginG");
   // No contact exist
   if (!condition) contactsView.render(undefined, condition, "load");
   // contacts exist
