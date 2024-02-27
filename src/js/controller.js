3787dbaba78ffdc782bbdceccf04063e883c9696
e1rhyme <emoakpo.onerhime@gmail.com>
2024-02-27 19:52:43 +0100

    still on mobile ready

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,7 +7,7 @@ import deleteContactView from "./view/deleteContactView.js";
 
 // Handles loading of contacts in the view
 function controlLoadContacts(condition) {
-  console.log("debugginG");
+  console.log("dEbugginG");
   // No contact exist
   if (!condition) contactsView.render(undefined, condition, "load");
   // contacts exist
